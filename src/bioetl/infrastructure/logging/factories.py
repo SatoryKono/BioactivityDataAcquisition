e4483d818c895fb5980a8600afbe6cc0cbaef03a
SatoryKono <821311@gmail.com>
2025-12-06 23:36:34 +0300

    Add domain hash service and fix logging contracts

--- src/bioetl/infrastructure/logging/factories.py
+++ src/bioetl/infrastructure/logging/factories.py
@@ -1,16 +1,20 @@
+import structlog
+
 from bioetl.domain.clients.base.logging.contracts import ProgressReporterABC
 from bioetl.domain.observability import LoggingPort
 from bioetl.infrastructure.logging.impl.progress_reporter import (
     TqdmProgressReporterImpl,
 )
-from bioetl.infrastructure.observability.factories import default_logging_port
+from bioetl.infrastructure.logging.impl.unified_logger import UnifiedLoggerImpl
+from bioetl.infrastructure.observability import factories as observability_factories
 
 
 def default_logger() -> LoggingPort:
     """
     Создает и конфигурирует логгер по умолчанию.
     """
-    return default_logging_port()
+    observability_factories._configure_structlog()
+    return UnifiedLoggerImpl(logger=structlog.get_logger())
 
 
 def default_progress_reporter() -> ProgressReporterABC:
