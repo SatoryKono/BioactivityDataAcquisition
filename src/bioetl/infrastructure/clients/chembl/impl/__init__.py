75265a5b73bd74a57873f01cb114c1840a32346a
SatoryKono <821311@gmail.com>
2025-12-09 12:14:35 +0300

    1

--- src/bioetl/infrastructure/clients/chembl/impl/__init__.py
+++ src/bioetl/infrastructure/clients/chembl/impl/__init__.py
@@ -1,5 +1,8 @@
 """
-ChEMBL implementation details.
+ChEMBL client implementations.
 """
 
-__all__ = []
+from .chembl_extraction_service_impl import ChemblExtractionServiceImpl
+from .http_client import ChemblApiPortImpl
+
+__all__ = ["ChemblExtractionServiceImpl", "ChemblApiPortImpl"]
