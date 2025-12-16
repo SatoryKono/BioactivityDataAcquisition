1a40ca4582cb01cb1226ab37e712307ccfb97317
SatoryKono <821311@gmail.com>
2025-12-16 13:41:02 +0300

    refactor: improve assertion messages in test_architecture.py for better clarity

--- src/bioetl/domain/__init__.py
+++ src/bioetl/domain/__init__.py
@@ -16,18 +16,16 @@ from bioetl.domain.types import (
 )
 
 __all__ = [
-    # Type aliases
-    "RunID",
-    "EntityID",
-    "ContentHash",
     "BatchID",
-    "Watermark",
-    # Enums
-    "RunType",
-    "DriftLevel",
-    "HealthStatus",
     "CircuitBreakerState",
-    "DataClassification",
-    "ErrorType",
+    "ContentHash",
     "DQStatus",
+    "DataClassification",
+    "DriftLevel",
+    "EntityID",
+    "ErrorType",
+    "HealthStatus",
+    "RunID",
+    "RunType",
+    "Watermark",
 ]
