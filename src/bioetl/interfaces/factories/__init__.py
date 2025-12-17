04c8d61513c6d8bd296509434cfcfc20db8ba4eb
SatoryKono <821311@gmail.com>
2025-12-17 18:35:25 +0300

    1

---
+++ src/bioetl/interfaces/factories/__init__.py
@@ -0,0 +1,5 @@
+"""Pipeline factories for the interfaces layer."""
+
+from bioetl.interfaces.factories.chembl_activity import ChEMBLActivityPipelineFactory
+
+__all__ = ["ChEMBLActivityPipelineFactory"]
