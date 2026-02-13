f32fb8321a9dc9f96ec830a5225f0f3a0739f8e5
SatoryKono <821311@gmail.com>
2026-02-13 21:30:45 +0300

    refactor(chembl): unify dto taxonomy and molecule field naming

--- src/bioetl/application/pipelines/chembl/activity_transformer.py
+++ src/bioetl/application/pipelines/chembl/activity_transformer.py
@@ -23,7 +23,7 @@ from bioetl.application.pipelines.chembl.base_chembl_transformer import (
 )
 from bioetl.domain.entities import Bioactivity
 from bioetl.domain.transformations import safe_float
-from bioetl.domain.value_objects import validate_taxonomy_id_str
+from bioetl.domain.value_objects import validate_taxonomy_id
 
 if TYPE_CHECKING:
     from bioetl.domain.types import BronzeRecord
@@ -71,8 +71,8 @@ _MOLECULE_TARGET_ASSAY = FieldGroup(
         # Standardized to 'taxonomy_id' for NCBI consistency (was 'tax_id')
         FieldSpec(
             "target_tax_id",
-            target="taxonomy_id",
-            converter=validate_taxonomy_id_str,
+            target="target_taxonomy_id",
+            converter=validate_taxonomy_id,
         ),
         *simple_fields(
             "assay_type",
