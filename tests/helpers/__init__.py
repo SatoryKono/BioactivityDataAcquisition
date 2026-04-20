6e8c51767ce63ceb2d2fb7b560c050e639d87e01
SatoryKono <821311@gmail.com>
2026-04-20 10:25:35 +0300

    1

--- tests/helpers/__init__.py
+++ tests/helpers/__init__.py
@@ -1,5 +1,20 @@
 """Test helper utilities."""
 
+from tests.helpers.artifact_generators import (
+    assert_build_artifacts_are_stable,
+    assert_check_artifacts_detects_drift,
+    assert_check_artifacts_passes_for_fresh_outputs,
+    assert_repeated_core_output_bytes_are_stable,
+    assert_written_core_artifacts_are_deterministic,
+)
 from tests.helpers.clock import FixedClock, StepClock
 
-__all__ = ["FixedClock", "StepClock"]
+__all__ = [
+    "FixedClock",
+    "StepClock",
+    "assert_build_artifacts_are_stable",
+    "assert_check_artifacts_detects_drift",
+    "assert_check_artifacts_passes_for_fresh_outputs",
+    "assert_repeated_core_output_bytes_are_stable",
+    "assert_written_core_artifacts_are_deterministic",
+]
