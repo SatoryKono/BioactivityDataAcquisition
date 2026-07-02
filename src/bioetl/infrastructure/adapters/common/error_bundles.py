8f1ef8fb347f55871d38243fdb4cc1230b2a06da
SatoryKono <821311@gmail.com>
2026-07-02 17:02:55 +0300

    1

--- src/bioetl/infrastructure/adapters/common/error_bundles.py
+++ src/bioetl/infrastructure/adapters/common/error_bundles.py
@@ -17,7 +17,24 @@ __all__ = [
     "COMMON_ADAPTER_HEALTH_ERRORS",
     "COMMON_ADAPTER_HEALTH_ERRORS_WITH_KEYERROR",
     "COMMON_TITLE_FALLBACK_ERRORS",
+    "build_common_network_error_bundle",
 ]
+
+
+def build_common_network_error_bundle(
+    *extra_errors: type[BaseException],
+) -> tuple[type[BaseException], ...]:
+    """Return the canonical network/runtime error bundle shared by adapters."""
+    return (
+        BioETLError,
+        NetworkError,
+        RequestError,
+        *extra_errors,
+        OSError,
+        ValueError,
+        TypeError,
+        RuntimeError,
+    )
 
 COMMON_ADAPTER_FETCH_RESILIENCE_ERRORS = (
     BioETLError,
@@ -33,38 +50,15 @@ COMMON_ADAPTER_FETCH_RESILIENCE_ERRORS = (
     Exception,
 )
 
-COMMON_ADAPTER_HEALTH_ERRORS = (
-    BioETLError,
-    NetworkError,
-    RequestError,
+COMMON_ADAPTER_HEALTH_ERRORS = build_common_network_error_bundle(
     HTTPStatusError,
-    OSError,
-    ValueError,
-    TypeError,
-    RuntimeError,
     Exception,
 )
 
-COMMON_ADAPTER_HEALTH_ERRORS_WITH_KEYERROR = (
-    BioETLError,
-    NetworkError,
-    RequestError,
+COMMON_ADAPTER_HEALTH_ERRORS_WITH_KEYERROR = build_common_network_error_bundle(
     HTTPStatusError,
-    OSError,
-    ValueError,
-    TypeError,
-    RuntimeError,
     KeyError,
     Exception,
 )
 
-COMMON_TITLE_FALLBACK_ERRORS = (
-    BioETLError,
-    NetworkError,
-    RequestError,
-    OSError,
-    ValueError,
-    TypeError,
-    RuntimeError,
-    KeyError,
-)
+COMMON_TITLE_FALLBACK_ERRORS = build_common_network_error_bundle(KeyError)
